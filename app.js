d95f893be35ff1fc07f78ec876e7ab43cadcdf6a
azizbek <azizbek>
2025-05-31 14:39:24 +0500

    intion commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const buttons = document.querySelectorAll(".button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const html = document.documentElement;
+    html.dataset.theme = button.dataset.theme;
+  });
+});
